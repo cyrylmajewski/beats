cb306a73b67ffbc6af00ebc60b1c2d29a888fe4d
CrackThis <prm1078life@gmail.com>
2020-06-25 01:45:40 +0200

    RWD Changes

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(){
-    if ($(window).scrollTop() > 10) {
-        $('.mobile-menu').addClass('scroll');
-    }
-    else {
-        $('.mobile-menu').removeClass('scroll');
-    }
-});
