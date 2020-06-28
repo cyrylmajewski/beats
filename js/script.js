816104f2728db2b31333d12a21e5367b27f10b31
CrackThis <prm1078life@gmail.com>
2020-06-28 12:02:20 +0200

    burger menu js

--- js/script.js
+++ js/script.js
@@ -6,12 +6,24 @@ const openHamburger = document.querySelector("#hamburger");
 const menu = document.querySelector(".menu__mobile");
 const cross = document.querySelector(".close");
 
+function toggleClass(elem, classElem) {
+    elem.classList.toggle(classElem);
+}
+
 openHamburger.addEventListener("click", e => {
     e.preventDefault();
-    menu.style.display = "block";
+    // menu.style.display = "block";
+    menu.classList.remove("hidden");
+    setTimeout(function() {
+        toggleClass(menu, "open");
+    }, 100);
 });
 
 cross.addEventListener("click", e => {
     e.preventDefault();
-    menu.style.display = "none";
+    // menu.style.display = "none";
+    menu.classList.remove("open");
+    setTimeout(function() {
+        toggleClass(menu, "hidden");
+    }, 1000);
 });
