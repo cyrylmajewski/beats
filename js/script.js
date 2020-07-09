150865c12b8dd36f1d45da3c83b3ef2c037f96e7
CrackThis <prm1078life@gmail.com>
2020-07-09 09:08:59 +0200

    week_4 changes

--- js/script.js
+++ js/script.js
@@ -162,15 +162,7 @@ $('.form').submit(e => {
                 to: to.val(),
             },
             
-            error: data => {
-                const message = data.responseJSON.message;
-                content.text(message);
-                modal.addClass("error-modal");
-                $.fancybox.open({
-                    src: "#modal",
-                    type: "inline"
-                });
-            }
+            
         });
 
         request.done(data => {
