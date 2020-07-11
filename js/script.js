2cae3ff6164820e83f35fbba7a3bef8a11e1fc11
CrackThis <prm1078life@gmail.com>
2020-07-11 18:51:54 +0200

    js color section mobile version

--- js/script.js
+++ js/script.js
@@ -48,6 +48,10 @@ $(document).ready(function(){
     
     classFunction();
     $(window).resize(classFunction);
+
+    $("body").on("scroll", function(e) {
+        console.log(event.deltaY);
+    });
    });
 
 
@@ -190,19 +194,109 @@ $(".app-close-modal").on("click", e => {
 });
 
 //COLOR SECTION
-$(".color__link").on("click", function(e) {
+
+const mesureWidth = item => {
+    const desktopWidth = $(window).width();
+    const list = item.closest(".color__list");
+    const link = list.find(".color__link");
+    const linkWidth = link.width();
+    const linksWidth = linkWidth * link.length;
+    const contentWidth = desktopWidth - linksWidth;
+    if(desktopWidth > 929) {
+        return 630;
+    }
+    else  if(desktopWidth > 480){
+        return contentWidth;
+    }
+    else {
+        return desktopWidth - linkWidth;
+    }
+};
+
+const openItem = item => {
+    const desktopWidth = $(window).width();
+    const desc = item.find(".color__desc");
+    const descWidth = mesureWidth(item);
+    desc.width(descWidth);
+    item.addClass("active");
+    if(item.siblings().hasClass("active")) {
+        closeItem(item.siblings());
+    }
+};
+
+const rightPos = (elem, pos) => {
+    return elem.css("right", pos);
+};
+
+const closeItem = item => {
+        const list = item.closest(".color__list"); 
+        const desc = item.find(".color__desc");
+        if($(window).width() <= 480) {
+            setTimeout(rightPos, 500, list, 0);
+        }
+        desc.width(0);
+        item.removeClass("active");
+};
+
+const addWidth = (block, width, item) => {
+    item.addClass("active");
+    return block.width(width);
+};
+
+const openMobileItem = (item, desktopWidth, linksSum) => {
+    const desc = item.find(".color__desc");
+    const list = item.closest(".color__list");
+    const link = item.find(".color__link");
+    const linkArr = linksSum.find(".color__link");
+    const linkWidth = item.width();
+    const descWidth = desktopWidth - linkWidth;
+    const linkArrLen = linkArr.length - 1;
+    for(let i = 0; i < linkArrLen + 1; i++) {
+        if(link[0] == linkArr[i]) {
+            let linkRight = -(linkWidth * (linkArrLen - i));
+            list.css("right", linkRight);
+            if(linkArr[i] == linkArr[linkArrLen]) {
+                addWidth(desc, descWidth, item);
+            }
+            else {
+                setTimeout(addWidth, 400, desc, descWidth, item);
+            }
+        }
+    }
+};
+
+$(".color__link").on("click", e => {
+    const desktopWidth = $(window).width();
     e.preventDefault();
-    var $this = $(this);
-    $this.next().toggleClass("color__open");
-    if($(".color__desc").hasClass("color__open")) {
-        $(".color__desc").removeClass("color__open");
-        $this.next().toggleClass("color__open");
+    const $this = $(e.currentTarget);
+    const item = $this.closest(".color__item");
+    const linksSum = $(".color__list").find(".color__item");
+    if(item.hasClass("active")) {
+        closeItem(item);
+    }
+    else {
+        if(desktopWidth > 480) {
+            openItem(item);
+        }
+        else {
+            openMobileItem(item, desktopWidth, linksSum);
+        }
     }
 });
 
-$(".color__close").on("click", function(e) {
+$(".color__close").on("click", e => {
     e.preventDefault();
-    var $this = $(this);
-    $this.closest(".color__desc").toggleClass("color__open");
+    const $this = $(e.currentTarget);
+    const item = $this.closest(".color__item");
+    if(item.hasClass("active")) {
+        closeItem(item);
+    }
 });
 
+//  WHEEL JS
+
+// $(document).ready(function() {
+//     $("body").on("scroll", function(e) {
+//         console.log(event.deltaY);
+//     });
+// });
