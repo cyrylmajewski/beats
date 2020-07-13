b999e63d381b0b2c79c30bf10a62522301326def
CrackThis <prm1078life@gmail.com>
2020-07-13 20:35:23 +0200

    yandex map API

--- js/script.js
+++ js/script.js
@@ -561,5 +561,41 @@ volumeButton.addEventListener("click", e => {
     video.volume = toggleVolume(video, volumeButton, volumePos);
 });
 
+//YANDEX MAP
+let myMap;
 
+const init = () => {
+    myMap = new ymaps.Map("map", {
+// Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [55.731475, 37.608342],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 14,
+            controls: []
+    });
 
+    const places = [
+        [55.723498, 37.610035], 
+        [55.736809, 37.618472], 
+        [55.731326, 37.636255]
+    ]; 
+
+    const myCollection = new ymaps.GeoObjectCollection({}, {
+        draggable: false,
+        iconLayout: 'default#image',
+        iconImageHref: "./img/icons/beats_map.png",
+        iconImageSize: [46, 57],
+        iconImageOffset: [-35, -52]
+    });
+    places.forEach(places => {
+        myCollection.add(new ymaps.Placemark(places));
+    });
+
+    myMap.geoObjects.add(myCollection);
+    myMap.behaviors.disable('scrollZoom');
+};
+
+ymaps.ready(init);
